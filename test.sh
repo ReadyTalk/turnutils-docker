4ecffcef3f6f38f93e03d31646262df45283638a
Andrew Suderman <andrew.suderman@readytalk.com>
2018-02-21 23:25:10 -0700

    Adding timeout var to test script

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ARGS="-T -v -n 1 -p 443 -u websip -w websip"
+TIMEOUT=60
 
 if [ -z "$1" -a ! -f '.config' ]; then
     echo "You need to specify an endpoint or provide a .config list of servers"
@@ -8,11 +9,11 @@ fi
 
 test() {
     docker rm -f turntest > /dev/null 2>&1
-    docker run --name turntest -e ARGS="$ARGS" -e TARGET=$host -itd readytalk/turnutils:latest > /dev/null 2>&1
+    docker run --name turntest -e TIMEOUT=$TIMEOUT -e ARGS="$ARGS" -e TARGET=$host -itd readytalk/turnutils:latest > /dev/null 2>&1
     exit_code=$(docker wait turntest)
 
     if [[ "$exit_code" != 0 ]]; then
-        echo "ERROR: $host server does not work"
+        (>&2 echo "ERROR: $host server does not work")
     else
         echo "SUCCESS: $host is working!"
     fi
